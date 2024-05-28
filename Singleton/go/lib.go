f9e10e4309c6559d1b021b07e1074b39ead36c7d
xianren <xianren711@qq.com>
2024-05-28 19:17:22 +0800

    update

--- Singleton/go/lib.go
+++ Singleton/go/lib.go
@@ -1,13 +1,21 @@
 package lib
 
+import (
+	"sync"
+)
+
 // 私有, 防止外部创建实例
 type singleton struct {
 }
 
+// 协程安全
+var mutex = &sync.Mutex{}
 var instance *singleton
 
 func NewSingleton() *singleton {
 	if instance == nil {
+		mutex.Lock()
+		defer mutex.Unlock()
 		instance = &singleton{}
 	}
 	return instance
